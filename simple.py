f05cf6bfe43760ff758a15565bc36f62235d7495
Andrew Denta <andrew.denta@gmail.com>
2016-04-03 00:01:19 -0400

    added working simple version of our game

--- simple.py
+++ simple.py
@@ -7,18 +7,24 @@ nextToadStep = {
 'left': (-1,0),
 'right': (1,0),
 'up': (0,1),
-'down':(0,-1)
+'down':(0,-1),
+'leftup': (-.5,.5),
+'rightup': (.5,.5),
+'leftdown': (-.5,-.5),
+'rightdown':(.5,-.5)
 
 }
 
-marioMoves = [0]
+steps = 0
+marioMoves = [0.5,-0.5]
 toadMoves = nextToadStep.keys()
 
 def dist(p1, p2):
     return math.sqrt((p2[0] - p1[0]) ** 2 + (p2[1] - p1[1]) ** 2)
 
-def makeToad():
+def makeToad(i):
     toad = {}
+    toad['id'] = i
     toad['x'] = 0.0
     toad['y'] = 0
     toad['distanceToMario'] = float('inf')
@@ -30,9 +36,11 @@ def makeToad():
 
 
 mario = [5.0,5.0]
-toads = [makeToad() for i in range(1)]
+toads = [makeToad(i) for i in range(4)]
 
 def turn():
+    mario[0] += choice(marioMoves)
+    mario[1] += choice(marioMoves)
 
     for toad in toads:
         toad['previousDistance'] = toad['distanceToMario']
@@ -44,7 +52,7 @@ def turn():
         toad['x'] += toadMove[0]
         toad['y'] += toadMove[1]
         print "toad moved",toad['direction']
-        print "toad is now at",[toad['x'],toad['y']],"and",toad['distanceToMario'],"away from mario\n"
+        print "toad",toad['id'],"is now at",[toad['x'],toad['y']],"and",toad['distanceToMario'],"away from mario\n"
 
 
 
@@ -56,12 +64,14 @@ def checkWinner():
     return capture
 
 
+
 print "mario is at",mario
 while not checkWinner():
 
-    sleep(.1)
+    sleep(.04)
     turn()
+    steps += 1
 
 
-print "toads:",toads
-print "Mario:",mario
+
+print "a toad found mario in",steps,"turns."
