68d4b9a04f4c6fcc76cbc647fdf35d5e769bb589
Andrew Denta <andrew.denta@gmail.com>
2016-04-13 23:00:27 -0400

    added functions for analysis that return if mario was caught in a trial, and how long it took. Drawing is corrupted. g

--- chase.py
+++ chase.py
@@ -30,18 +30,9 @@ moves = {
 'right and down':(1,-1)
 
 }
-toadCount = 4
-steps = 0
-timer = 150
 
 def dist(p1, p2):
     return math.sqrt((p2[0] - p1[0]) ** 2 + (p2[1] - p1[1]) ** 2)
-
-def drawTime():
-    remaining = timer - steps
-    minutes = str(remaining // 60)
-    seconds = str(remaining % 60)
-    return minutes + ":" + seconds + "."
 
 def makeToad(i):
     toad = {}
@@ -53,97 +44,111 @@ def makeToad(i):
     toad['direction'] = choice(moves.keys())
     return toad
 
+def trial(toadCount, timer,delay):
+    steps = 0
+    mario = [0,0]
 
-steps = 0
-mario = [0,0]
-while True:
-    mario = [choice(range(BOARDSIZE)),choice(range(BOARDSIZE))]
-    if(board[mario[0]][mario[1]] is not boardUtils.wall):
-        break
-toads = [makeToad(i) for i in range(toadCount)]
-
-def turn():
-    board[mario[0]][mario[1]]=boardUtils.marioTrail
-    #Mario AI
-    closestToad = 0
-    for toad in toads:
-        if dist(mario,[toad['x'],toad['y']]) > closestToad:
-            closestToad = toad
-    #print closestToad['x']
-    #print closestToad['y']
-    #print dist(mario,[closestToad['x'],closestToad['y']])
-    xdiff = mario[0]-closestToad['x']
-    ydiff = mario[1]-closestToad['y']
-    if abs(xdiff) < abs(ydiff):
-        if xdiff > 0:
-            dx = 1
-            dy = 0
-        else:
-            dx = -1
-            dy = 0
-    elif ydiff > xdiff:
-        if ydiff > 0:
-            dy = 1
-            dx = 0
-        else:
-            dy = -1
-            dx = 0
-    else:
-        move =  moves[choice(moves.keys())]
-        dx = move[0]
-        dy = move[1]
     while True:
-        if not( board[mario[0] + dx][ mario[1] + dy]==boardUtils.wall):
-            mario[0] += dx
-            mario[1] += dy
+        mario = [choice(range(BOARDSIZE)),choice(range(BOARDSIZE))]
+        if(board[mario[0]][mario[1]] is not boardUtils.wall):
             break
-        move =  moves[choice(moves.keys())]
-        dx = move[0]
-        dy = move[1]
+    toads = [makeToad(i) for i in range(toadCount)]
 
-    board[mario[0]][mario[1]]=boardUtils.mario
-
-    #Toad AI
-    for toad in toads:
-        toad['previousDistance'] = toad['distanceToMario']
-        toad['distanceToMario'] = dist(mario,[toad['x'],toad['y']])
-
-        if toad['previousDistance'] < toad['distanceToMario']:
-            toad['direction'] = choice(moves.keys())
-        toadMove = moves[toad['direction']]
-
-        board[toad['x']][toad['y']]=boardUtils.toadTrail
-        if not( board[toad['x'] + toadMove[0]][ toad['y'] + toadMove[1]]==boardUtils.wall):
-            toad['x'] += toadMove[0]
-            toad['y'] += toadMove[1]
-
-        board[toad['x']][toad['y']]=boardUtils.toad
-
-        print "toad moved",toad['direction']
-        print "toad",toad['id'],"is now at",[toad['x'],toad['y']],"and",toad['distanceToMario'],"away from mario,",mario,"\n"
-
-    print "Time:",drawTime()
-    boardUtils.drawBoard(board)
+    def drawTime():
+        remaining = timer - steps
+        minutes = str(remaining // 60)
+        seconds = str(remaining % 60)
+        return minutes + ":" + seconds + "."
 
 
-def checkCapture():
-    capture = False
-    for toad in toads:
-        capture = capture or toad['x'] == mario[0] and toad['y'] == mario[1]
-    return capture
+    def turn():
+        board[mario[0]][mario[1]]=boardUtils.marioTrail
+        #Mario AI
+        closestToad = 0
+        for toad in toads:
+            if dist(mario,[toad['x'],toad['y']]) > closestToad:
+                closestToad = toad
+        #print closestToad['x']
+        #print closestToad['y']
+        #print dist(mario,[closestToad['x'],closestToad['y']])
+        xdiff = mario[0]-closestToad['x']
+        ydiff = mario[1]-closestToad['y']
+        if abs(xdiff) < abs(ydiff):
+            if xdiff > 0:
+                dx = 1
+                dy = 0
+            else:
+                dx = -1
+                dy = 0
+        elif ydiff > xdiff:
+            if ydiff > 0:
+                dy = 1
+                dx = 0
+            else:
+                dy = -1
+                dx = 0
+        else:
+            move =  moves[choice(moves.keys())]
+            dx = move[0]
+            dy = move[1]
+        while True:
+            if not( board[mario[0] + dx][ mario[1] + dy]==boardUtils.wall):
+                mario[0] += dx
+                mario[1] += dy
+                break
+            move =  moves[choice(moves.keys())]
+            dx = move[0]
+            dy = move[1]
+
+        board[mario[0]][mario[1]]=boardUtils.mario
+
+        #Toad AI
+        for toad in toads:
+            toad['previousDistance'] = toad['distanceToMario']
+            toad['distanceToMario'] = dist(mario,[toad['x'],toad['y']])
+
+            if toad['previousDistance'] < toad['distanceToMario']:
+                toad['direction'] = choice(moves.keys())
+            toadMove = moves[toad['direction']]
+
+            board[toad['x']][toad['y']]=boardUtils.toadTrail
+            if not( board[toad['x'] + toadMove[0]][ toad['y'] + toadMove[1]]==boardUtils.wall):
+                toad['x'] += toadMove[0]
+                toad['y'] += toadMove[1]
+
+            board[toad['x']][toad['y']]=boardUtils.toad
+            if delay is not 0:
+                print "toad moved",toad['direction']
+                print "toad",toad['id'],"is now at",[toad['x'],toad['y']],"and",toad['distanceToMario'],"away from mario,",mario,"\n"
+        if delay is not 0:
+            print "Time:",drawTime()
+            boardUtils.drawBoard(board)
 
 
+    def checkCapture():
+        capture = False
+        for toad in toads:
+            capture = capture or toad['x'] == mario[0] and toad['y'] == mario[1]
+        return capture
 
-print "mario is at",mario
-while True:
-    sleep(.15)
-    turn()
-    steps += 1
 
-    if checkCapture():
-        print "a toad found mario in",steps,"turns."
-        break;
+    while True:
+        sleep(delay)
+        turn()
+        steps += 1
 
-    if steps >= timer:
-        print "Mario wins!"
-        break
+        if checkCapture():
+            if delay is not 0:
+                print "a toad found mario in",steps,"turns."
+            isCaptured = True
+            break;
+
+        if steps >= timer:
+            if delay is not 0:
+                print "Mario wins!"
+            isCaptured = False
+            break
+    return (isCaptured,steps)
+
+for i in range(100):
+    print trial(4,15,.05)
