2d9cea39541ab93d58fc7735696a181935021f5c
Andrew Denta <andrew.denta@gmail.com>
2016-04-25 19:10:45 -0400

    added more data files

--- chaseRunner.py
+++ chaseRunner.py
@@ -14,7 +14,7 @@ times= []
 #Trialrunner
 for i in range(trials):
     try:
-        res = chase.trial(4,25,0,marioAI,toadAI)
+        res = chase.trial(4,150,0,marioAI,toadAI)
     except:
         res = [False, 150]
     times.append(res[1])
@@ -26,4 +26,4 @@ print "Mario AI", marioAI
 print "Toad AI", toadAI
 print trials, "Trials:"
 print "Mean Game Time:", numpy.mean(times)
-print "Toad Win %",  (float(toad_win_count)/float(trials))*100 
+print "Toad Win %",  (float(toad_win_count)/float(trials))*100
